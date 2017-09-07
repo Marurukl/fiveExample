21a8bcf0e553a7cd0941359f32e238711ec4c591
lRaika <arnurelnur@gmail.com>
2017-09-07 16:42:29 +0600

    second

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            Square square = new Square(size);
+            Random rnd = new Random();
+            for (int i= 0; i < size; i++)
+            {
+                square[i] = rnd.Next(1, 10);
+                Console.Write("{0}\t",square[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Square
+    {
+        public Square(int size)
+        {
+            arr = new int[size];
+        }
+
+        int[] arr;
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+
+                double a= Math.Pow(value , 2);
+
+                arr[index] = (int)a;
+                
+            }
+        }
+    }
+}
